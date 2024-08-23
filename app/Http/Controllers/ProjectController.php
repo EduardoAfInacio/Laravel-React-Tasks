dd8b1814ed8711968b97c967d906c587221f3b5a
EduardoAfInacio <eduafinacio@gmail.com>
2024-08-23 06:58:31 -0300

    Ajusting ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
-        $projects = $query->paginate(5);
+        $projects = $query->paginate(5)->onEachSide(1);
 
         return inertia('Project/Index', [
             "projects" => ProjectResource::collection($projects)
