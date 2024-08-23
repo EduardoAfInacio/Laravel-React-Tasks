ca8a04f60ab95a2fff269b95afd13930f14f7896
EduardoAfInacio <eduafinacio@gmail.com>
2024-08-23 07:45:41 -0300

    Criando UserResource e adaptando o ProjectResource ao mesmo.

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -22,10 +22,14 @@ class ProjectResource extends JsonResource
             "description"=> $this->description,
             "created_at"=> (new Carbon($this->created_at))->format("Y-m-d"),
             "due_date"=> (new Carbon($this->due_date))->format("Y-m-d"),
-            "status"=>$this->status,
-            "image_path"=>$this->image_path,
-            "created_by"=>$this->created_by,
-            "updated_by"=>$this->updated_by,
+            "status"=> $this->status,
+            "image_path"=> $this->image_path,
+            "created_by"=> new UserResource($this->createdBy),
+            "updated_by"=> new UserResource($this->updatedBy),
+            //Ao passar dados para o front utilizando uma intancia dessa resource, apareceria
+            //todas as informacoes de quem criou e quem atualizou o projeto, logo, eh necessario criar
+            //uma resource para o usuario e instancia-lo aqui nos campos created e updated.
+            //para controlarmos tudo que aparece.
         ];
     }
 }
