7d384ad4173ed34b092509b1572af9b50cfc539c
EduardoAfInacio <eduafinacio@gmail.com>
2024-09-01 22:23:23 -0300

    Adding filtering to Project index

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,10 +16,17 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
+        if(request()->has("name")){
+            $query = $query->where("name","like","%".request("name")."%");
+        }
+        if(request()->has("status")){
+            $query = $query->where("status", request("status"));
+        }
         $projects = $query->paginate(5)->onEachSide(1);
 
         return inertia('Project/Index', [
-            "projects" => ProjectResource::collection($projects)
+            "projects" => ProjectResource::collection($projects),
+            "queryParams" => request()->query() ?:null,
         ]);
     }
 
